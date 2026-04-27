17a76ed080e835f50e0dbe7e2f9710cd44a7762c
хуймэ <usimovd@gmail.com>
2026-04-27 12:44:19 +0300

    add gender field

---
+++ pb_migrations/1777282647_updated_products.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:109810d717ddca3e250d995bbf738c8685284414f70432b1fb6a4882b2ef4b07
+size 649
---
+++ pb_migrations/1777282692_updated_products.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:95c2d7e14481b947a7c7787ce9f6845b09ac6291f45ea08b1eb639bfb7674c18
+size 899
